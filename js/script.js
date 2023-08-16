df0be150b009e5034063b60772fdd79134ed836d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-08-16 09:42:28 +0200

    Update script.js with comments
    Update script.js with comments for better readability.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Create pop-up with message:
 alert('Hello world');
 
+// Create variable and display its value as content on .html file
 let favoriteFood = 'pizza';
 
 document.write(favoriteFood);
