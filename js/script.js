c9c393e748cbb6831b854f7c828aeea657f4d666
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-08-18 15:21:38 +0200

    Delete content of script.js file
    Delete all existing code of script.js file.

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Create pop-up with message:
-alert('Hello world');
-
-// Create variable and display its value as content on .html file
-let favoriteFood = 'pizza';
-
-document.write(favoriteFood);
