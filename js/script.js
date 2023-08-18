a9c2a021509ee24143a19fdabb54f423e8804fe9
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-08-18 16:47:30 +0200

    Update script.js  with array
    Create array  with 3  objects through `let` keyword.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Create `pokemonList` Array with 3 Pokémon objects through `let` keyword
+
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['monster', 'grass']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['field', 'fairy']
+    },
+    {
+        name: 'Horsea',
+        height: 0.4,
+        type: ['water', 'dragon']
+    }
+];
