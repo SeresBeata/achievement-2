8e2879a4b1149607dac4d3e9f90f783c7a6d08a0
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-08-20 16:50:33 +0200

    Update script.js with for loop & conditional
    Create for loop for "pokemonList" array, with conditional statement to display different data on index.html file.

--- js/script.js
+++ js/script.js
@@ -27,4 +27,27 @@ let pokemonList = [
     //Assign submitted Pokémon name as a value to `currentPokemonName` variable
     let currentPokemonName = document.querySelector('#pokemon_name').value;
 
+    // Create for loop for `pokemonList` Array
+    for (let index = 0; index < pokemonList.length; index++) {
+
+        //Create conditional statement to display different key-value pairs on index.html file
+        if(pokemonList[index].name.includes(currentPokemonName) === true){
+
+            //Display submitted Pokémon name on result section of index.html file
+            document.querySelector('#name_key').innerText = currentPokemonName;
+            //Display type of the submitted Pokémon on result section of index.html file
+            document.querySelector('#type').innerText = pokemonList[index].type[0] + ', ' +pokemonList[index].type[1];
+            //Create conditional statments to display different messages depending on the value of the `height`of the submitted Pokémon
+            if(pokemonList[index].height >= 0.7){
+                document.querySelector('#height').innerText = pokemonList[index].height + ' - It is a large Pokémon!';
+            }else if (pokemonList[index].height < 0.7 && pokemonList[index].height > 0.4){
+                document.querySelector('#height').innerText = pokemonList[index].height + ' - It is an average sized Pokémon!';
+            }else{
+                document.querySelector('#height').innerText = pokemonList[index].height + ' - It is a small Pokémon!';
+            }
+
+        }else{
+            alert('Sorry, there is no such a Pokémon, try again!')
+        }
+    }
  });
